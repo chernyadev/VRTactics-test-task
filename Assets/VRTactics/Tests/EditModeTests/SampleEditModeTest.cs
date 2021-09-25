937a3ef756910fec9795fc11507193084778a259
nchernyadev <nikita.chernyadev@gmail.com>
2021-09-25 17:13:29 +0300

    Sample edit- and playmode tests added

---
+++ Assets/VRTactics/Tests/EditModeTests/SampleEditModeTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace VRTactics.EditModeTests
+{
+    public class SampleEditModeTest
+    {
+        [Test]
+        public void SampleEditModeTestPasses()
+        {
+        }
+    }
+}
---
+++ Assets/VRTactics/Tests/PlayModeTests/SamplePlaymodeTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using UnityEngine.TestTools;
+
+namespace VRTactics.PlayModeTests
+{
+    public class SamplePlaymodeTest
+    {
+        [UnityTest]
+        public IEnumerator SamplePlayModeTestPasses()
+        {
+            yield return null;
+        }
+    }
+}
