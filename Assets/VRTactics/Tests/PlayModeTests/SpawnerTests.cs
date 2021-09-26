1143b67c92d1f8f86bf2fc80361cfddd8caf7eca
nchernyadev <nikita.chernyadev@gmail.com>
2021-09-26 14:38:48 +0300

    Added playmode tests for spawner

---
+++ Assets/VRTactics/Tests/PlayModeTests/SpawnerTests.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using VRTactics.Utils;
+using VRTactics.Utils.Tests;
+
+namespace VRTactics.PlayModeTests
+{
+    public class SpawnerTests
+    {
+        [UnityTest]
+        public IEnumerator Spawner_destroys_instantiated_objects()
+        {
+            const int objectsCountToSpawn = 5;
+
+            var prefab = new GameObject("Prefab");
+            var spawnPoint = new GameObject("sp").transform;
+            var spawner = new GameObject("Spawner").AddComponent<Spawner>();
+
+            SpawnerTestUtils.ConfigureSpawner(spawner, prefab, new[] {spawnPoint});
+
+            var spawnedObjects = spawner.SpawnCollection(objectsCountToSpawn);
+            spawner.Clean();
+
+            yield return null;
+
+            foreach (var t in spawnedObjects)
+            {
+                Assert.IsTrue(t == null);
+            }
+        }
+    }
+}
