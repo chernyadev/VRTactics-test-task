6d999adafcc65e28fdfa9b0cbb07d1d6cd954699
nchernyadev <nikita.chernyadev@gmail.com>
2021-09-26 18:36:54 +0300

    Test for enemy attack added

--- Assets/VRTactics/Tests/PlayModeTests/GameplayTests.cs
+++ Assets/VRTactics/Tests/PlayModeTests/GameplayTests.cs
@@ -1,55 +1,103 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
+using VRTactics.Characters;
+using VRTactics.Characters.FSM;
 using VRTactics.GameManagement;
 
 namespace VRTactics.PlayModeTests
 {
     public class GameplayTests
     {
+        private CharacterController _playerController;
+        private GameManager _gameManager;
+        private DestinationTrigger _destination;
+        private List<DetectionController> _enemies;
+
         [UnityTest]
         [LoadScene("Assets/VRTactics/Scenes/GameplayScene.unity")]
         public IEnumerator Game_ends_when_player_reaches_destination()
         {
-            yield return new WaitForSeconds(0.1f);
-
-            var playerController = Object.FindObjectOfType<CharacterController>();
-            var gameManager = Object.FindObjectOfType<GameManager>();
-            var destination = Object.FindObjectOfType<DestinationTrigger>();
+            yield return AssignGameplayReferences();
 
             var gameFinished = false;
-            gameManager.onGameFinished.AddListener(_ => gameFinished = true);
+            _gameManager.onGameFinished.AddListener(_ => gameFinished = true);
 
-            playerController.enabled = false;
-            playerController.transform.position = destination.transform.position;
-            playerController.enabled = true;
+            TeleportPlayer(_destination.transform.position);
 
             yield return new WaitForFixedUpdate();
 
             Assert.IsTrue(gameFinished);
         }
-        
+
         [UnityTest]
         [LoadScene("Assets/VRTactics/Scenes/GameplayScene.unity")]
-        public IEnumerator Game_failed_when_player_reaches_destination_and_doesnt_find_enemies()
+        public IEnumerator Game_failed_if_player_reaches_destination_and_does_not_find_enemies()
         {
-            yield return new WaitForSeconds(0.1f);
-
-            var playerController = Object.FindObjectOfType<CharacterController>();
-            var gameManager = Object.FindObjectOfType<GameManager>();
-            var destination = Object.FindObjectOfType<DestinationTrigger>();
+            yield return AssignGameplayReferences();
 
             var gameResultsData = new GameResultsData();
-            gameManager.onGameFinished.AddListener(result => gameResultsData = result);
+            _gameManager.onGameFinished.AddListener(result => gameResultsData = result);
 
-            playerController.enabled = false;
-            playerController.transform.position = destination.transform.position;
-            playerController.enabled = true;
+            TeleportPlayer(_destination.transform.position);
 
             yield return new WaitForFixedUpdate();
 
             Assert.AreEqual(OverallGameResult.Defeat, gameResultsData.OverallResult);
+        }
+
+        [UnityTest]
+        [LoadScene("Assets/VRTactics/Scenes/GameplayScene.unity")]
+        public IEnumerator Game_failed_if_player_is_attacked()
+        {
+            yield return AssignGameplayReferences();
+
+            var gameResultsData = new GameResultsData();
+            _gameManager.onGameFinished.AddListener(result => gameResultsData = result);
+
+            foreach (var enemy in _enemies)
+            {
+                var fsm = enemy.GetComponent<EnemyStateMachine>();
+                var serializedFsm = new SerializedObject(fsm);
+
+                var idleState = (IdleState) serializedFsm.FindProperty("idleState").objectReferenceValue;
+                var serializedState = new SerializedObject(idleState);
+                serializedState.FindProperty("minAttackDelay").floatValue = 0;
+                serializedState.FindProperty("maxAttackDelay").floatValue = 0;
+
+                serializedState.ApplyModifiedProperties();
+                serializedFsm.ApplyModifiedProperties();
+                
+                idleState.OnEnter();
+            }
+
+            var targetEnemyTransform = _enemies.First().transform;
+            TeleportPlayer(targetEnemyTransform.position + targetEnemyTransform.forward * 1.0f);
+
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.AreEqual(OverallGameResult.Defeat, gameResultsData.OverallResult);
+        }
+
+        private IEnumerator AssignGameplayReferences()
+        {
+            yield return new WaitForSeconds(0.1f);
+
+            _playerController = Object.FindObjectOfType<CharacterController>();
+            _gameManager = Object.FindObjectOfType<GameManager>();
+            _destination = Object.FindObjectOfType<DestinationTrigger>();
+            _enemies = Object.FindObjectsOfType<DetectionController>().ToList();
+        }
+
+        private void TeleportPlayer(Vector3 targetPosition, Quaternion targetRotation = default)
+        {
+            _playerController.enabled = false;
+            _playerController.transform.SetPositionAndRotation(targetPosition, targetRotation);
+            _playerController.enabled = true;
         }
     }
 }
