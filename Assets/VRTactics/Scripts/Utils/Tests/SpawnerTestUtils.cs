b3cdb504877ffbbe50c9ad5eb846f5aa4f201dc1
nchernyadev <nikita.chernyadev@gmail.com>
2021-09-26 14:37:33 +0300

    Added spawner testing utils

---
+++ Assets/VRTactics/Scripts/Utils/Tests/SpawnerTestUtils.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace VRTactics.Utils.Tests
+{
+    public class SpawnerTestUtils
+    {
+        public static void ConfigureSpawner(Spawner spawner, GameObject prefab, Transform[] spawnPoints)
+        {
+            var serializedSpawner = new SerializedObject(spawner);
+
+            var prefabProp = serializedSpawner.FindProperty("prefab");
+            var spawnPointsProp = serializedSpawner.FindProperty("spawnPoints");
+
+            prefabProp.objectReferenceValue = prefab;
+            spawnPointsProp.arraySize = spawnPoints.Length;
+            for (var i = 0; i < spawnPoints.Length; i++)
+            {
+                spawnPointsProp.GetArrayElementAtIndex(i).objectReferenceValue = spawnPoints[i];
+            }
+
+            serializedSpawner.ApplyModifiedProperties();
+        }
+    }
+}
