e8919864efa023ada1da2089e4ef8effa57821a1
nchernyadev <nikita.chernyadev@gmail.com>
2021-09-26 18:09:20 +0300

    Added custom attribute to load scenes in playmode tests

---
+++ Assets/VRTactics/Tests/PlayModeTests/LoadSceneAttribute.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace VRTactics.PlayModeTests
+{
+    public class LoadSceneAttribute : NUnitAttribute, IOuterUnityTestAction
+    {
+        private readonly string _scenePath;
+        private EditorBuildSettingsScene[] _originalScenesInBuild;
+
+        public LoadSceneAttribute(string scenePath)
+        {
+            _scenePath = scenePath;
+        }
+
+        public IEnumerator BeforeTest(ITest test)
+        {
+            _originalScenesInBuild = EditorBuildSettings.scenes;
+            var sceneSettings = new EditorBuildSettingsScene[1];
+            var sceneToAdd = new EditorBuildSettingsScene(_scenePath, true);
+            sceneSettings[sceneSettings.Length - 1] = sceneToAdd;
+            EditorBuildSettings.scenes = sceneSettings;
+            
+            yield return EditorSceneManager.LoadSceneAsyncInPlayMode(_scenePath, new LoadSceneParameters(LoadSceneMode.Single));
+        }
+
+        public IEnumerator AfterTest(ITest test)
+        {
+            EditorBuildSettings.scenes = _originalScenesInBuild;
+            yield return null;
+        }
+    }
+}
